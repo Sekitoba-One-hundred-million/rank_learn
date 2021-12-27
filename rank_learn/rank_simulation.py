7f552e48880ac6cb3c9f86b7841c43ec876b582a
石川幹晴 <kansei2@kansei2.local>
2021-12-27 13:54:54 +0900

    rank buy

--- rank_learn/rank_simulation.py
+++ rank_learn/rank_simulation.py
@@ -16,7 +16,7 @@ def standardization( data ):
     conv = 0
 
     for d in data:
-        conv = math.pow( ave - d, 2 )
+        conv = math.pow( d - ave, 2 )
 
     conv /= len( data )
     conv = math.sqrt( conv )
@@ -63,9 +63,6 @@ def main( model, data ):
             ex_value["score"] = score
             ex_value["rank"] = data[race_id][horce_id]["answer"]["rank"]
             ex_value["odds"] = data[race_id][horce_id]["answer"]["odds"]
-            #ex_value["rate"] = 0
-            #ex_value["ex"] = 0
-            #ex_value["horce_id"] = horce_id
             score_list.append( score )
             horce_list.append( ex_value )
 
@@ -76,33 +73,19 @@ def main( model, data ):
         
         sort_result = sorted( horce_list, key=lambda x:x["score"], reverse = True )
         max_rate = 0
-        #bet_rate = ( max( score_list ) * ( bet_horce["odds"] + 1 ) - 1 ) / bet_horce["odds"]
-        #bet_money = int( money * bet_rate / 200 ) * 100
-        #money -= bet_money
+
         for i in range( 0, len( sort_result ) ):
-            key_rank = str( int( sort_result[i]["rank"] ) )
-            lib.dic_append( test, key_rank, { "data": 0, "count": 0 } )
-            test[key_rank]["data"] += sort_result[i]["score"]
-            test[key_rank]["count"] += 1
-            #lib.log.write( "rank:{} odds:{} score:{}".format( str( sort_result[i]["rank"] ), str( sort_result[i]["odds"] ), str( sort_result[i]["score"] )) )
-
-        #lib.log.write( "---------" )
-
-        bet_horce = sort_result[0]
-        #ex_value = score_list[i] * bet_horce["odds"]
-        #rate = score_list
-
-        if bet_horce["odds"] > 50:
-            continue
-
-        test_result["count"] += 1
+            if sort_result[i]["score"] <= 0:
+                continue
+            
+            bet_horce = sort_result[i]
+            test_result["count"] += 1
         
-        if bet_horce["rank"] == 1:
-            #money += bet_money * bet_horce["odds"]
-            recovery_rate += bet_horce["odds"]
-            test_result["win"] += 1
-            test_result["money"] += bet_horce["odds"]
-            lib.log.write( "odds:" + str( bet_horce["odds"] ) + " score:" + str( max( score_list ) ) )
+            if bet_horce["rank"] == 1:
+                recovery_rate += bet_horce["odds"]
+                test_result["win"] += 1
+                test_result["money"] += bet_horce["odds"]
+                lib.log.write( "odds:" + str( bet_horce["odds"] ) + " score:" + str( max( score_list ) ) )
 
     recovery_rate = test_result["money"] / test_result["count"]
     recovery_rate *= 100
@@ -116,8 +99,4 @@ def main( model, data ):
     lib.log.write( "勝率{}%".format( win_rate ) )
     lib.log.write( "賭けた回数{}回".format( test_result["count"] ) )
 
-    for i in range( 1, 19 ):
-        k = str( i )
-        test[k]["data"] /= test[k]["count"]
-        print( "rank:{} score:{}".format( k, test[k]["data"] ) )
     return recovery_rate, win_rate
